b17d46672d0c08b869e8ff3c3b21d42afa4163d1
Yoshiiro <59020911+Yoshiiro07@users.noreply.github.com>
2023-12-02 13:53:03 -0300

    starter project
    starter project

---
+++ backend/src/test/java/com/teste/boxlocks/BoxlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.boxlocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
